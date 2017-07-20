0d77c1256002b8c27b8b693ee3a7a72f28d38fb9
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-07-19 23:32:02 -0400

    gogo

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+
+   attr_accessor :name, :roster
+
+   def initialize(name)
+     @name = name
+     @roster = {}
+   end
+
+   def add_student(name, grade)
+     roster[grade] = [] unless roster[grade]
+     roster[grade] << name
+   end
+
+   def grade(num)
+     roster[num]
+   end
+
+   def sort
+     sort_students = {}
+     roster.each do |grade, names|
+       sort_students[grade] = names.sort
+     end
+     sort_students
+   end
+ end
